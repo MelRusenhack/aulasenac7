5a61a793b42abf50765da158f531091c6649c8c8
MelRusenhack <melissa.rusenhack@gmail.com>
2023-09-05 19:55:20 -0300

    origin

---
+++ menu.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset= "UFT-8">
+        <title> Menu Principal </title>
+    </head>
+    <body>
+        <h1> Menu Principal </h1>
+        <hr>
+
+        <div class="container">
+            <a href="frmcadaluno.php" class="btn btn-success"> Cadastrar Aluno </a> <br>
+            <a href= "frmcadcliente.php"> Cadastrar Cliente </a> <br>
+            <a href="frmcadfornecedor.php"> Cadastrar Fornecedor </a> <br>
+
+        </div>
+
+    </body>
+</html>
